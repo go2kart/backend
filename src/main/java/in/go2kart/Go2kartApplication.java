13c0ab35331c73b368b331c7091e49c02376e393
manishdotkr <officialmanishkr98@gmail.com>
2024-11-03 21:25:06 +0530

    add: added java files for basic springboot app

---
+++ src/main/java/in/go2kart/Go2kartApplication.java
@@ -0,0 +1,13 @@
+package in.go2kart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Go2kartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Go2kartApplication.class, args);
+	}
+
+}
